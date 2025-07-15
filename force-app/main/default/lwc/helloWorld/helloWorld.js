f2e88a1fd21da39d279de7b74df521a74639ce3b
rupeshDev <rupesh.chougule3067@gmail.com>
2025-07-15 13:59:35 +0530

    lwc demo

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,11 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'World';
+  greeting = 'Salesforce';
   changeHandler(event) {
     this.greeting = event.target.value;
+  }
+
+  connectedCallback() {
+    window.alert("Hello "+this.greeting);
   }
 }
